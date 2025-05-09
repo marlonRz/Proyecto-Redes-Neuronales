cff5399faa3a318415e9be1c07eb539d3e7cc8b9
Marlon Stiven <marlonsteven930@gmail.com>
2025-05-09 18:42:01 -0500

    commit4

--- parte4.py
+++ parte4.py
@@ -8,24 +8,24 @@ import numpy as np
 # Define a transform to normalize the data
 transform = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # 3 channels for RGB
 ])
 
 # Download and Load the training data
-trainset = datasets.FashionMNIST('F_NNIST_data/', download=True, train=True, transform=transform)
+trainset = datasets.CIFAR10('CIFAR10_data/', download=True, train=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
 # Download and Load the test data
-testset = datasets.FashionMNIST('F_NNIST_data/', download=True, train=False, transform=transform)
+testset = datasets.CIFAR10('CIFAR10_data/', download=True, train=False, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
 
-# Building the network
+# Building the network (adjusted for 32x32 RGB images)
 class Network(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(784, 256)
+        self.fc1 = nn.Linear(32*32*3, 256)  # 32x32 pixels x 3 channels
         self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 10)
+        self.fc3 = nn.Linear(128, 10)  # 10 classes in CIFAR-10
         
     def forward(self, x):
         x = x.view(x.shape[0], -1)
@@ -63,16 +63,16 @@ for e in range(epochs):
 def view_classify(img, ps):
     """Function for viewing an image and it's predicted classes."""
     ps = ps.data.numpy().squeeze()
-    img = img.numpy().squeeze()
+    img = img.numpy().squeeze().transpose(1, 2, 0)  # Adjust for color image
     
     fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
-    ax1.imshow(img, cmap='gray')
+    ax1.imshow((img * 0.5 + 0.5))  # Unnormalize
     ax1.axis('off')
     ax2.barh(np.arange(10), ps)
     ax2.set_aspect(0.1)
     ax2.set_yticks(np.arange(10))
-    ax2.set_yticklabels(['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-                       'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'])
+    ax2.set_yticklabels([
+    'avión', 'automóvil', 'pájaro', 'gato', 'ciervo', 'perro', 'rana', 'caballo', 'barco', 'camión'])
     ax2.set_title('Class Probability')
     ax2.set_xlim(0, 1.1)
     plt.tight_layout()
@@ -80,10 +80,10 @@ def view_classify(img, ps):
 
 # Get a batch of test images
 dataiter = iter(testloader)
-images, labels = next(dataiter)  # Changed from dataiter.next() to next(dataiter)
+images, labels = next(dataiter)
 img = images[0]
-# Convert 2D image to 1D vector
-img = img.view(1, 784)
+# Convert 3D image to 1D vector
+img = img.view(1, 32*32*3)
 
 # Calculate the class probabilities (softmax) for img
 with torch.no_grad():
@@ -91,4 +91,4 @@ with torch.no_grad():
 ps = F.softmax(logits, dim=1)
 
 # Plot the image and probabilities
-view_classify(img.view(1, 28, 28), ps)
+view_classify(img.view(1, 3, 32, 32), ps)
